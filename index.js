d1d394633be46fe39a0aa061a7c7537011671109
sanju2728 <sanjanabhat2002@gmail.com>
2023-04-13 20:52:50 +0530

    minor changes

--- index.js
+++ index.js
@@ -25,17 +25,17 @@ app.get('/fact', (req, res) => {
 
 //route handler for GET requests to /picture
 app.get('/picture', (req, res) => {
-    fs.readFile('./pics.txt', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal server error');
-            return;
-        }
-        const pictures = data.split("\n");
+    // fs.readFile('./pics.txt', 'utf-8', (err, data) => {
+    //     if (err) {
+    //         console.error(err);
+    //         res.status(500).send('Internal server error');
+    //         return;
+    //     }
+        //const pictures = data.split("\n");
         //res.send(sendRandomItem(pictures));
         res.send('https://cdn2.thecatapi.com/images/3g4.jpg');
         
     })
-})
+//})
 
 app.listen(80);
